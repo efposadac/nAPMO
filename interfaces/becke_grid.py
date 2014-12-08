54b11ef9e3ead6b00965acd784d28c4dd68ac718
Fernando Posada <efposadac@unal.edu.co>
2014-12-08 21:53:27 +0100

    Added construction for becke grids and calculation of becke weights. Added numerical integration test. Improved numerical procedures.

--- interfaces/becke_grid.py
+++ interfaces/becke_grid.py
@@ -16,38 +16,100 @@ class BeckeGrid(object):
     see: Becke, A. D. A multicenter numerical integration scheme for polyatomic 
     molecules. J. Chem. Phys. 88, 2547 (1988).
     """
-    def __init__(self, n_radial=15, n_angular=110):
+    def __init__(self, n_radial=15, n_angular=110, position=[0.,0.,0.]):
         super(BeckeGrid, self).__init__()
 
-        self.radial = n_radial
-        self.angular = n_angular
-        self.size = self.radial * self.angular
+        self.data={}
+        self.data['n_radial'] = n_radial
+        self.data['n_angular'] = n_angular
+        self.data['size'] = n_radial * n_angular
 
-        self.x = np.zeros(self.size)
-        self.y = np.zeros(self.size)
-        self.z = np.zeros(self.size)
-        self.w = np.zeros(self.size)
+        self.data['x'] = np.zeros(self.data['size'], dtype=np.float64)
+        self.data['y'] = np.zeros(self.data['size'], dtype=np.float64)
+        self.data['z'] = np.zeros(self.data['size'], dtype=np.float64)
+        self.data['w'] = np.zeros(self.data['size'], dtype=np.float64)
 
-    def radial_distr(self, n):
-    	""" Computes the radial distribution for a Becke-like grid (uses Chebyshev-Gauss method.)
+        # Radial distribution
+        q_r, w_r = nint.chebgauss_t_radial_q_w(n_radial)
 
-    	n : number of radial points.
-    	"""
-        r, w = nint.chebgauss_transformed(n)
+        #angular distribution
+        x, y, z, w_a = nint.lebedev_q_w(n_angular)
+    
+        count = 0
+        for i in xrange(n_radial):
+            solid_angle = 8.0 * np.arccos(0.0) * q_r[i] * q_r[i]
+            for j in xrange(n_angular):
+                self.data['x'][count] = x[j] * q_r[i] 
+                self.data['y'][count] = y[j] * q_r[i] 
+                self.data['z'][count] = z[j] * q_r[i] 
+                self.data['w'][count] = w_a[j] * w_r[i]  * solid_angle
+                count += 1
 
-        # domain transformation ... (check)
-        w = (w/(1.-r))/np.log(2.)
-    	r = 1.- np.log(1.-r)/np.log(2.0)
+        for i in xrange(self.get('size')):
+            self.data['x'][i]+= position[0]
+            self.data['y'][i]+= position[1]
+            self.data['z'][i]+= position[2]
 
-    	return r, w
 
-    def angular_distr(self, n):
-		""" Computes the angular distribution for a Becke-like grid 
-		see: V.I. Lebedev, and D.N. Laikov, Doklady Mathematics, 59, No. 3, 477 (1999)
-		x, y, z: Cartesian coordinates
-		w: weights
-		n : number of angular points
-		"""
+    def weights(self, particle_stack, particleID):
+        """Computes the Becke weights as described in:
+        Becke, A. D. A multicenter numerical integration scheme for polyatomic 
+        molecules. J. Chem. Phys. 88, 2547 (1988).
+        particle_stack: stack of particles. i.e. stack of e-
+        """
+        def cutoff_profile(mu):
+            """Iterated cutoff profile. eq. 21
+            """
+            return 0.5 * mu * ( 3.0 - (mu * mu))
 
-		x, y, z, w = nint.lebedev_weights(n)
-		return x, y, z, w
+        distance_to_particle = np.zeros(particle_stack.size(), dtype=np.float64)
+         
+        for point in xrange(self.get('size')):
+            cell_function = np.ones(particle_stack.size(), dtype=np.float64)
+            #Distance from grid point i to particle (distance_to_particle) (eq. 11)
+            for i in xrange(particle_stack.size()):
+                distance_to_particle[i] = np.sqrt( 
+                (self.get('x')[point] - particle_stack.get(i).get('position')[0])**2 +
+                (self.get('y')[point] - particle_stack.get(i).get('position')[1])**2 +
+                (self.get('z')[point] - particle_stack.get(i).get('position')[2])**2, dtype=np.float64 )
+
+            for i in xrange(1, particle_stack.size()):
+                for j in xrange(i):         
+                    #Internuclear distance (R_ij eq. 11)
+                    R_ij = np.sqrt(
+                    (particle_stack.get(j).get('position')[0] - particle_stack.get(i).get('position')[0])**2 +
+                    (particle_stack.get(j).get('position')[1] - particle_stack.get(i).get('position')[1])**2 +
+                    (particle_stack.get(j).get('position')[2] - particle_stack.get(i).get('position')[2])**2 )
+             
+                    # \mu_ij eq. 11
+                    mu_ij = (distance_to_particle[i] - distance_to_particle[j]) / R_ij                         
+                    
+                    #missing atomic size adjustment.
+             
+                    #f_k(\mu_ij) K = 3 eq. 20
+                    f_ij = cutoff_profile(cutoff_profile(cutoff_profile(mu_ij)))
+             
+                    #Cutoff profile s_k(\mu_ij) eq. 21
+                    s_ij = 0.5 * (1.0 - f_ij)
+
+                    #Cell function P_i(r) eq. 13
+                    cell_function[i] *=  s_ij            
+       
+            #cell_function sum denominator eq. 22
+            cell_function_sum = 0.0
+            for i in xrange(particle_stack.size()):
+                cell_function_sum += cell_function[i]
+       
+            #Weight calculation for each point eq. 22
+            self.get('w')[point] *= cell_function[particleID]/cell_function_sum
+
+    def get(self, key):
+        """Returns the value stored in key
+        """
+        assert type(key) == type('str')
+
+        try:
+            return self.data[key]
+        except KeyError:
+            raise
+
--- utilities/numerical_integration.py
+++ utilities/numerical_integration.py
@@ -6,9 +6,9 @@
 # efposadac@sissa.it 
 from __future__ import division
 import numpy as np
-from utilities import lebedev
+import utilities.lebedev as lebedev
 
-def chebgauss_transformed(n):
+def chebgauss_t_q_w(n):
 	"""Computes the abscissas and weights for transformed Gauss-Chebyshev quadrature of second kind.
 	
 	Special case for the integral I f(x) dx. since Gauss-Chebyshev is for I f(x) sqrt(1-x^2). 
@@ -18,6 +18,9 @@ def chebgauss_transformed(n):
 
 	returns the weights and abscissas to perform integration
 	"""
+
+	assert n > 0
+
 	w = np.zeros(n, dtype=np.float64)
 	r = np.zeros(n, dtype=np.float64)
 
@@ -36,6 +39,41 @@ def chebgauss_transformed(n):
 
 	return r, w
 
+def chebgauss_t_radial_q_w(n):
+	"""Computes the abscissas and weights for transformed Gauss-Chebyshev quadrature of second kind.
+	
+	Special case for the integral I f(x) dx. since Gauss-Chebyshev is for I f(x) sqrt(1-x^2). 
+
+	see: Perez-Jorda, J., San-Fabian, E. & Moscardo, F. A simple, reliable and efficient scheme for 
+	automatic numerical integration. Comput. Phys. Commun. 70, 271-284 (1992).
+
+	returns the weights and abscissas to perform integration (radial interval)
+	"""
+
+	assert n > 0
+
+	w = np.zeros(n, dtype=np.float64)
+	r = np.zeros(n, dtype=np.float64)
+
+	count = 1
+	for i in xrange(int((n+1)/2)):
+		d = n + 1.
+		s = np.sin((count*np.pi)/d)
+		c = np.cos((count*np.pi)/d)
+		s2 = s*s
+		s4 = s2 * s2
+		r[i] = 1. + (2./np.pi) * (1. + (2./3.) * s2) * c * s - (2. * count)/d
+		r[n-i-1] = -r[i]
+		w[i] = 16./(3.*d) * s4
+		w[n-i-1] = w[i]
+		count += 1
+
+	w = (w/(1.-r))/np.log(2.)
+	r = 1.- np.log(1.-r)/np.log(2.)
+
+	return r, w
+
+
 def chebgauss_integrate(f, eps=1.0e-10, max_iter=10):
 	"""Computes the integral of f(x) by using Gauss-Chebyshev quadrature of second kind.
 
@@ -48,27 +86,28 @@ def chebgauss_integrate(f, eps=1.0e-10, max_iter=10):
 	return: output type tuple with integral value and error.
 	""" 
 
-	n = 3
-	err = 1.0
+	n = 5
+	err = np.float64(1.0)
 	count = 0
-	aux = 0.0
+	aux = np.float64(0.0)
+
 	while err > eps:				
-		r, w = chebgauss_transformed(n)
-		out = 0.0
+		r, w = chebgauss_t_q_w(n)
+		out = np.float64(0.0)
 		for i in xrange(n):
 			out += f(r[i]) * w[i]
 
 		err = np.abs(out - aux)
 		count += 1
 		if count > max_iter : 
-			print 'Convergence not reached!'
+			print 'Convergence failed!'
 			break
 		n =  n + n + 1
 		aux = out
 
 	return out, err
 
-def lebedev_weights(n):
+def lebedev_q_w(n):
 	"""Computes the Lebedev points and weights for spherical integration.
 
 	see: V.I. Lebedev, and D.N. Laikov, Doklady Mathematics, 59, No. 3, 477 (1999)
@@ -76,15 +115,17 @@ def lebedev_weights(n):
 	x, y, z: Cartesian coordinates
 	w: weights		
 	"""
+
+	assert n > 0
+
 	x = np.zeros(n, dtype=np.float64)
 	y = np.zeros(n, dtype=np.float64)
 	z = np.zeros(n, dtype=np.float64)
 	w = np.zeros(n, dtype=np.float64)
-
-	if lebedev.lebedev_compute(x, y, z, w, n):
-		return x, y, z, w
+		
+	if lebedev.lebedev_compute(x, y, z, w, n) != 0:
+		return x, y, z, w		
 	else:
-		print "ERROR!!! Invalid number of angular points."
-		print "Info: Only supported:..."
+		print "Info: Number of angular points supported:..."
 		print "Info: 6,14,26,38,50,74,86,110,146,170,194,230,266,302,350,434,590,770"
-		return 0, 0, 0, 0
+		raise ValueError('Invalid number of angular points', n)
