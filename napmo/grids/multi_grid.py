5f9ffce1dab997ddcd30f54a5f647395d7d00d6c
Fernando Posada <efposadac@gmail.com>
2021-03-18 14:14:41 -0400

    fixed bug in multigrid

--- napmo/grids/multi_grid.py
+++ napmo/grids/multi_grid.py
@@ -9,138 +9,138 @@ import numpy as np
 
 import napmo
 
+
 class MultiGrid(object):
-	"""
-	This class handles multiple BeckeGrids. 
+    """
+    This class handles multiple BeckeGrids.
 
-	Sometimes it is important to know the common points between molecular grids
-	specifically in the case of multi-species calculations.
+    Sometimes it is important to know the common points between molecular grids
+    specifically in the case of multi-species calculations.
 
-	Args:
-		nspecies (int): Total number of unique species in the system
+    Args:
+            nspecies (int): Total number of unique species in the system
 
-	"""
-	def __init__(self, nspecies):
-		super(MultiGrid, self).__init__()
-		
-		assert isinstance(nspecies, int)
+    """
 
-		self._grids = {}
-		self._this = napmo.cext.MultiGrid_new(nspecies)
+    def __init__(self, nspecies):
+        super(MultiGrid, self).__init__()
 
-	def add_grid(self, grid):
-		"""
-		Adds a grid to the class
+        assert isinstance(nspecies, int)
 
-		Args:
-			grid (BeckeGrid): Grid to be added, must be of the type of ``BeckeGrid``.
-		"""
-		assert isinstance(grid, napmo.BeckeGrid)
-		
-		# append grid
-		grid_id = napmo.cext.MultiGrid_add_grid(self._this, grid._this)
-		self._grids[grid._symbol] = {"id": grid_id, "obj": grid}
+        self._grids = {}
+        self._this = napmo.cext.MultiGrid_new(nspecies)
 
+    def add_grid(self, grid):
+        """
+        Adds a grid to the class
 
-	def get_grid(self, symbol="", gid=-1):
-		"""
-		Returns the grid with a specific symbol or ID
+        Args:
+                grid (BeckeGrid): Grid to be added, must be of the type of
+                ``BeckeGrid``.
+        """
+        assert isinstance(grid, napmo.BeckeGrid)
 
-		Args:
-			symbol (str): Symbol of the desired grid
-			gid (int): ID of the grid
+        # append grid
+        grid_id = napmo.cext.MultiGrid_add_grid(self._this, grid._this)
+        self._grids[grid._symbol] = {"id": grid_id, "obj": grid}
 
-		Notes: 
-			One of the two arguments have to be provided, ``symbol`` has priority over ``gid``
-			if both are provided.
-		"""
-		assert isinstance(symbol, str)
-		assert isinstance(gid, int)
+    def get_grid(self, symbol="", gid=-1):
+        """
+        Returns the grid with a specific symbol or ID
 
-		# Symbol case
-		aux = self._grids.get(symbol, None)
-		if aux:
-			return aux.get('obj', None)
+        Args:
+                symbol (str): Symbol of the desired grid
+                gid (int): ID of the grid
 
-		# Id case
-		for grid in self._grids.values():
-			if grid.get('id', -2) == gid:
-				return grid.get('obj', None)
+        Notes:
+                One of the two arguments have to be provided, ``symbol``
+                has priority over ``gid`` if both are provided.
+        """
+        assert isinstance(symbol, str)
+        assert isinstance(gid, int)
 
-		# Default case
-		return aux
+        # Symbol case
+        aux = self._grids.get(symbol, None)
+        if aux:
+            return aux.get('obj', None)
 
-	def get_grid_id(self, symbol):
-		"""
-		Returns the ID for a given grid
+        # Id case
+        for grid in self._grids.values():
+            if grid.get('id', -2) == gid:
+                return grid.get('obj', None)
 
-		Args:
-			symbol (str): Symbol of the desired grid
-		"""
-		assert isinstance(symbol, str)
+        # Default case
+        return aux
 
-		aux = self._grids.get(symbol, None)
+    def get_grid_id(self, symbol):
+        """
+        Returns the ID for a given grid
 
-		if aux:
-			return aux.get('id', None)
-		return aux		
+        Args:
+                symbol (str): Symbol of the desired grid
+        """
+        assert isinstance(symbol, str)
 
-	def get_common_points(self, symbol_a, symbol_b):
-		"""
-		Returns the points with same coordinates between two grids.
+        aux = self._grids.get(symbol, None)
 
-		Args:
-			symbol_a (str): Symbol of the species ``a``.
-			symbol_b (str): Symbol of the species ``b``.
+        if aux:
+            return aux.get('id', None)
+        return aux
 
-		Returns:
-			output (ndarray): Array with the index in which the cartesian points are the same.
-			The size of the array is the number of common points ``n`` with the shape ``(n, 2)``
-			in which ``[:,0]`` corresponds to de index for ``a`` while ``[:,1]`` are the index for ``b``.
-		"""
-		assert isinstance(symbol_a, str)
-		assert isinstance(symbol_b, str)
+    def get_common_points(self, symbol_a, symbol_b):
+        """
+        Returns the points with same coordinates between two grids.
 
-		idx_a = self.get_grid_id(symbol_a)
-		idx_b = self.get_grid_id(symbol_b)
+        Args:
+                symbol_a (str): Symbol of the species ``a``.
+                symbol_b (str): Symbol of the species ``b``.
 
-		# Is the same grid
-		if idx_a == idx_b:
-			return np.arange(self.get_grid(symbol_a).size)
-		
-		# Uses C function
-		if idx_a != None and idx_b != None:
-			ptr = napmo.cext.MultiGrid_get_common_index(self._this, idx_a, idx_b)
-			size = napmo.cext.MultiGrid_get_common_index_size(self._this, idx_a, idx_b)
-			return np.ctypeslib.as_array(ptr, shape=(size,2))
+        Returns:
+                output (ndarray): Array with the index in which the cartesian
+                points are the same. The size of the array is the number of
+                common points ``n`` with the shape ``(n, 2)`` in which
+                ``[:,0]`` corresponds to de index for ``a`` while ``[:,1]``
+                are the index for ``b``.
+        """
+        assert isinstance(symbol_a, str)
+        assert isinstance(symbol_b, str)
 
-		# No grids for such species (or one of them)
-		else:
-			return None
+        idx_a = self.get_grid_id(symbol_a)
+        idx_b = self.get_grid_id(symbol_b)
 
-	def show(self):
-		"""
-		Shown information about the object
-		"""
-		print("\nNumber of Grids: ", self.ngrids)
-		print("Object Information: ", self._grids)
+        # Is the same grid
+        if idx_a == idx_b:
+            return np.arange(self.get_grid(symbol_a).size)
 
-	@property
-	def ngrids(self):
-		"""
-		Returns the number of the grids in the object
-		"""
-		return napmo.cext.MultiGrid_get_ngrids(self._this)
+        # Uses C function
+        if idx_a is not None and idx_b is not None:
+            ptr = napmo.cext.MultiGrid_get_common_index(
+                self._this, idx_a, idx_b)
+            size = napmo.cext.MultiGrid_get_common_index_size(
+                self._this, idx_a, idx_b)
+            return np.ctypeslib.as_array(ptr, shape=(size, 2))
 
-	@property
-	def nspecies(self):
-		"""
-		Returns the number of species used to create the object
-		"""
-		return napmo.cext.MultiGrid_get_nspecies(self._this)
+        # No grids for such species (or one of them)
+        else:
+            return None
 
+    def show(self):
+        """
+        Shown information about the object
+        """
+        print("\nNumber of Grids: ", self.ngrids)
+        print("Object Information: ", self._grids)
 
-	
-	
+    @property
+    def ngrids(self):
+        """
+        Returns the number of the grids in the object
+        """
+        return napmo.cext.MultiGrid_get_ngrids(self._this)
 
-	
+    @property
+    def nspecies(self):
+        """
+        Returns the number of species used to create the object
+        """
+        return napmo.cext.MultiGrid_get_nspecies(self._this)
